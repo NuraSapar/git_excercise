7cc717857520707a30924638803a4003d866f522
Nurasapar <nurzhamal.sapar@gmail.com>
2022-04-28 21:22:11 -0400

    commit

---
+++ sample1.py
@@ -0,0 +1,8 @@
+# sample file for git
+print("Hello world!")
+# How to tell git not to share certain files
+# create a new file with the name .gitignore and add filenames you want to hide
+print("more lines in the sample")
+for i in range():
+    print(f"numbers:{i}")
+# HW Register in the GitHub with your email and password
